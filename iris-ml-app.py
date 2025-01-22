b36ecbd7bd717f5ab86af72d084c53651c6354dd
Farviz14 <2301186I@student.tp.edu.sg>
2025-01-22 11:10:03 +0800

    Update iris-ml-app.py

--- iris-ml-app.py
+++ iris-ml-app.py
@@ -40,7 +40,7 @@ flat_model = st.sidebar.selectbox("Flat Model", [
     'MODEL A-MAISONETTE', 'MAISONETTE', 'IMPROVED-MAISONETTE', 'APARTMENT',
     'TERRACE', 'PREMIUM APARTMENT', '2-ROOM', 'MULTI GENERATION'
 ])
-lease_remaining = st.sidebar.slider("Lease Remaining (Years)", min_value=70, max_value=99, step=1)
+lease_age_years = st.sidebar.slider("Lease Remaining (Years)", min_value=70, max_value=99, step=1)
 storey_category = st.sidebar.selectbox("Storey Category", ["Low Storey", "Mid Storey", "High Storey"])
 
 # Mapping towns to regions
